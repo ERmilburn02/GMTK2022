ceec4a6f9f8182702412b4c27b75d714d25acd6a
ERmilburn02 <61363431+ERmilburn02@users.noreply.github.com>
2022-07-16 17:48:49 +0100

    remove logs in release builds

--- gmtk_2022_game/game/src/main.cpp
+++ gmtk_2022_game/game/src/main.cpp
@@ -1,8 +1,13 @@
 #include "Core/App.hpp"
 #include "Core/LoadingScreen.hpp"
 
+#include <raylib.h>
+
 int main(int argc, char** argv)
 {
+#if defined(NDEBUG)
+	SetTraceLogLevel(LOG_ERROR);
+#endif
 	gmtk2022::App* app = new gmtk2022::App;
 	app->ChangeScreen(new gmtk2022::LoadingScreen(app));
 	app->Start();
